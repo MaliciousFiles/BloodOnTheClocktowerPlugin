a841d5925dd9ecd3cd77e4225ba6a6d08f11403b
Malcolm Roalson <mtroalson@gmail.com>
2024-12-17 19:55:06 -0600

    Initial commit

---
+++ src/main/java/io/github/maliciousfiles/bloodOnTheClocktower/BloodOnTheClocktower.java
@@ -0,0 +1,17 @@
+package io.github.maliciousfiles.bloodOnTheClocktower;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BloodOnTheClocktower extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
