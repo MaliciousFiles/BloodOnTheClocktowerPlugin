042117cdd00ba8929137c1ebf78eafc11834feca
Malcolm Roalson <mtroalson@gmail.com>
2024-12-28 01:02:01 -0600

    push zip?

--- src/main/java/io/github/maliciousfiles/bloodOnTheClocktower/play/ResourcePackHandler.java
+++ src/main/java/io/github/maliciousfiles/bloodOnTheClocktower/play/ResourcePackHandler.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class ResourcePackHandler implements Listener {
 
     private static final UUID uuid = UUID.fromString("25abeb60-a254-43ab-bb92-d5ec2268ecae"); // randomly generated
-    private static final byte[] hash = HexFormat.of().parseHex("c4e8bcd06ee5cefef3003c3c48b8ea99467870a0"); // SHA1 sum of zip file
+    private static final byte[] hash = HexFormat.of().parseHex("b079714ab1535f338b44676200c2a8480d44bd87"); // SHA1 sum of zip file
     @EventHandler
     public void onJoin(PlayerJoinEvent evt) {
         evt.getPlayer().addResourcePack(uuid,
