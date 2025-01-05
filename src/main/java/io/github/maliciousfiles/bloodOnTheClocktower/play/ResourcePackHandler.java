dbe89d86d6764b4f50810b2856741c97faec1b1f
Malcolm Roalson <mtroalson@gmail.com>
2025-01-04 19:47:28 -0600

    fix resource pack

--- src/main/java/io/github/maliciousfiles/bloodOnTheClocktower/play/ResourcePackHandler.java
+++ src/main/java/io/github/maliciousfiles/bloodOnTheClocktower/play/ResourcePackHandler.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class ResourcePackHandler implements Listener {
 
     private static final UUID uuid = UUID.fromString("25abeb60-a254-43ab-bb92-d5ec2268ecae"); // randomly generated
-    private static final byte[] hash = HexFormat.of().parseHex("1813a5e0e2a4e60e0c27486d9807d18b9fd050e7"); // SHA1 sum of zip file
+    private static final byte[] hash = HexFormat.of().parseHex("a606cadb03a49f7bb9b493474d3e17c49053d7b3"); // SHA1 sum of zip file
     @EventHandler
     public void onJoin(PlayerJoinEvent evt) {
         evt.getPlayer().addResourcePack(uuid,
